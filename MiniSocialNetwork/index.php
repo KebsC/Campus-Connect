7e2211c1f1808e35a110524fb0a14de314a17009
KebsC <kebssnot@gmail.com>
2026-03-31 22:31:18 +0800

    Fix require path for UserModel in index.php

--- MiniSocialNetwork/index.php
+++ MiniSocialNetwork/index.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-require_once __DIR__ . '/../app/models/UserModel.php';
+require_once __DIR__ . '/app/models/UserModel.php';
 require_once __DIR__ . '/../app/controllers/AuthController.php';
 require_once __DIR__ . '/../app/controllers/FeedController.php';
 require_once __DIR__ . '/../config/database.php';
